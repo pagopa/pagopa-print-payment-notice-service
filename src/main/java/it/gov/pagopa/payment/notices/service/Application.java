878b58e762fee6394504cbce772e563d1e9a0b36
Alessio Cialini <alessio.cialini@emeal.nttdata.com>
2024-04-23 16:34:22 +0200

    [VAS-828] feat: fixed errors

--- src/main/java/it/gov/pagopa/payment/notices/service/Application.java
+++ src/main/java/it/gov/pagopa/payment/notices/service/Application.java
@@ -2,7 +2,6 @@ package it.gov.pagopa.payment.notices.service;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.kafka.test.context.EmbeddedKafka;
 import org.springframework.scheduling.annotation.EnableAsync;
 
 @SpringBootApplication
