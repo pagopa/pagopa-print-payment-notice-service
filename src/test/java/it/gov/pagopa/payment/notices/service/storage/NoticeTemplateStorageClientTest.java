16725af8feb5609b3d5e18c8abf986ca7add9bd3
Alessio Cialini <alessio.cialini@emeal.nttdata.com>
2024-04-23 15:48:21 +0200

    [VAS-826] feat: Updated tests to increase coverage

--- src/test/java/it/gov/pagopa/payment/notices/service/storage/NoticeTemplateStorageClientTest.java
+++ src/test/java/it/gov/pagopa/payment/notices/service/storage/NoticeTemplateStorageClientTest.java
@@ -3,7 +3,9 @@ package it.gov.pagopa.payment.notices.service.storage;
 import com.azure.core.util.Context;
 import com.azure.storage.blob.BlobClient;
 import com.azure.storage.blob.BlobContainerClient;
+import com.azure.storage.blob.models.BlobStorageException;
 import com.azure.storage.blob.options.BlobDownloadToFileOptions;
+import it.gov.pagopa.payment.notices.service.exception.AppException;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
@@ -13,10 +15,10 @@ import java.io.File;
 import java.time.Duration;
 
 import static org.junit.jupiter.api.Assertions.assertNotNull;
+import static org.junit.jupiter.api.Assertions.assertThrows;
 import static org.mockito.ArgumentMatchers.any;
 import static org.mockito.ArgumentMatchers.anyString;
-import static org.mockito.Mockito.doReturn;
-import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.*;
 
 @ExtendWith(MockitoExtension.class)
 class NoticeTemplateStorageClientTest {
@@ -31,7 +33,7 @@ class NoticeTemplateStorageClientTest {
         blobContainerClient = mock(BlobContainerClient.class);
         noticeTemplateStorageClient = new NoticeTemplateStorageClient(true, blobContainerClient);
         blobClientMock = mock(BlobClient.class);
-        doReturn(blobClientMock).when(blobContainerClient).getBlobClient(anyString());
+        lenient().doReturn(blobClientMock).when(blobContainerClient).getBlobClient(anyString());
     }
 
     @Test
@@ -44,6 +46,30 @@ class NoticeTemplateStorageClientTest {
                 );
         File result = noticeTemplateStorageClient.getTemplate("testFile");
         assertNotNull(result);
+    }
+
+    @Test
+    void shouldReturnException() {
+        doThrow(new BlobStorageException("test", null, null)).when(blobClientMock)
+                .downloadToFileWithResponse(
+                        any(BlobDownloadToFileOptions.class),
+                        any(Duration.class),
+                        any(Context.class)
+                );
+        assertThrows(AppException.class, () -> noticeTemplateStorageClient.getTemplate("testFile"));
+    }
+
+    @Test
+    void shouldReturnExceptionOnMissingClient() {
+        assertThrows(AppException.class, () ->
+                new NoticeTemplateStorageClient(false, null)
+                        .getTemplate("testFile"));
+    }
+
+    @Test
+    void shouldReturnKOOnWrongFile() {
+        assertThrows(AppException.class, () ->
+                noticeTemplateStorageClient.getTemplate("../../testFile"));
     }
 
 }
