1d195afdd87a7c8152b346950eab2af1c6d45fe8
Alessio Cialini <alessio.cialini@emeal.nttdata.com>
2024-05-13 16:23:14 +0200

    [VAS-831] feat: introducing NoticeGenerationServiceImplTest, NoticesController, AppError

--- src/main/java/it/gov/pagopa/payment/notices/service/exception/AppError.java
+++ src/main/java/it/gov/pagopa/payment/notices/service/exception/AppError.java
@@ -7,7 +7,7 @@ import org.springframework.http.HttpStatus;
 @Getter
 public enum AppError {
     INTERNAL_SERVER_ERROR(HttpStatus.INTERNAL_SERVER_ERROR, "Internal Server Error", "Something was wrong"),
-    BAD_REQUEST(HttpStatus.INTERNAL_SERVER_ERROR, "Bad Request", "%s"),
+    BAD_REQUEST(HttpStatus.BAD_REQUEST, "Bad Request", "%s"),
     UNAUTHORIZED(HttpStatus.UNAUTHORIZED, "Unauthorized", "Error during authentication"),
     FORBIDDEN(HttpStatus.FORBIDDEN, "Forbidden", "This method is forbidden"),
     RESPONSE_NOT_READABLE(HttpStatus.BAD_GATEWAY, "Response Not Readable", "The response body is not readable"),
