4f4961f86e5f15f433201c8f561f1f771f7e562b
Jacopo <jacopo.carlini@gmail.com>
2024-05-14 18:04:47 +0200

    fix generator

--- src/main/java/it/gov/pagopa/payment/notices/service/client/NoticeGenerationClient.java
+++ src/main/java/it/gov/pagopa/payment/notices/service/client/NoticeGenerationClient.java
@@ -1,10 +1,10 @@
 package it.gov.pagopa.payment.notices.service.client;
 
+import feign.Response;
 import it.gov.pagopa.payment.notices.service.model.NoticeGenerationRequestItem;
 import org.springframework.cloud.openfeign.FeignClient;
 import org.springframework.http.MediaType;
-import org.springframework.web.bind.annotation.GetMapping;
-import feign.Response;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestParam;
 
@@ -16,11 +16,12 @@ public interface NoticeGenerationClient {
 
     /**
      * Generate a single PDF Payment Notice
-     * @param folderId folderId to use for saving the produced pdf (optional)
+     *
+     * @param folderId                    folderId to use for saving the produced pdf (optional)
      * @param noticeGenerationRequestItem generation data, containing a templateId and notice data
      * @return generated pdf
      */
-    @GetMapping(value = "/notices/generate", produces = MediaType.APPLICATION_JSON_VALUE)
+    @PostMapping(value = "/notices/generate", produces = MediaType.APPLICATION_JSON_VALUE)
     Response generateNotice(
             @RequestParam(value = "folderId", required = false) String folderId,
             @RequestBody NoticeGenerationRequestItem noticeGenerationRequestItem
