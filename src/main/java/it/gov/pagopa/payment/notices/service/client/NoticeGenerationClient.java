dc8664be9a1923cfc2f4f9692efa6a582f778c4c
Jacopo <jacopo.carlini@gmail.com>
2024-05-15 15:35:00 +0200

    subkey

--- src/main/java/it/gov/pagopa/payment/notices/service/client/NoticeGenerationClient.java
+++ src/main/java/it/gov/pagopa/payment/notices/service/client/NoticeGenerationClient.java
@@ -1,6 +1,7 @@
 package it.gov.pagopa.payment.notices.service.client;
 
 import feign.Response;
+import it.gov.pagopa.payment.notices.service.config.feign.NoticeFeignConfig;
 import it.gov.pagopa.payment.notices.service.model.NoticeGenerationRequestItem;
 import org.springframework.cloud.openfeign.FeignClient;
 import org.springframework.http.MediaType;
@@ -11,7 +12,7 @@ import org.springframework.web.bind.annotation.RequestParam;
 /**
  * Feign client instance to be used to contact notices-generation APIs
  */
-@FeignClient(name = "notice-generation", url = "${rest-client.notice-generation.base-url}")
+@FeignClient(name = "notice-generation", url = "${rest-client.notice-generation.base-url}", configuration = NoticeFeignConfig.class)
 public interface NoticeGenerationClient {
 
     /**
