d9a6c3d3c5ef95371ad7a4c1d3c83168b73297e4
Alessio Cialini <alessio.cialini@emeal.nttdata.com>
2024-05-31 13:04:54 +0200

    feat: Updated models

--- src/main/java/it/gov/pagopa/payment/notices/service/model/institutions/UploadData.java
+++ src/main/java/it/gov/pagopa/payment/notices/service/model/institutions/UploadData.java
@@ -3,6 +3,7 @@ package it.gov.pagopa.payment.notices.service.model.institutions;
 import io.swagger.v3.oas.annotations.media.Schema;
 import jakarta.validation.constraints.NotEmpty;
 import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Size;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
@@ -17,28 +18,32 @@ public class UploadData {
     @Schema(description = "CI tax code", requiredMode = Schema.RequiredMode.REQUIRED)
     @NotNull
     @NotEmpty
+    @Size(max = 16)
     private String taxCode;
 
     @Schema(description = "CI full name", requiredMode = Schema.RequiredMode.REQUIRED)
     @NotNull
     @NotEmpty
+    @Size(max = 50)
     private String fullName;
 
     @Schema(description = "CI organization unit managing the payment")
+    @Size(max = 50)
     private String organization;
 
     @Schema(description = "CI info", requiredMode = Schema.RequiredMode.REQUIRED)
     @NotNull
     @NotEmpty
+    @Size(max = 100)
     private String info;
 
     @Schema(description = "Boolean to refer if it has a web channel", requiredMode = Schema.RequiredMode.REQUIRED)
-    @NotNull
-    private Boolean webChannel;
+    @Builder.Default
+    private Boolean webChannel = false;
 
     @Schema(description = "Boolean to refer if it has a app channel", requiredMode = Schema.RequiredMode.REQUIRED)
-    @NotNull
-    private Boolean appChannel;
+    @Builder.Default
+    private Boolean appChannel = false;
 
     @Schema(description = "CI physical channel data")
     private String physicalChannel;
