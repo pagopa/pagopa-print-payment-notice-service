6e479d9ac58c93894127d18d26b220a655e123c8
Alessio Cialini <alessio.cialini@emeal.nttdata.com>
2024-06-26 17:11:19 +0200

    [VAS-1145] feat: Updated NoticeGenerationServiceImpl and NoticeGenerationServiceImplTest

--- src/main/java/it/gov/pagopa/payment/notices/service/service/impl/NoticeGenerationServiceImpl.java
+++ src/main/java/it/gov/pagopa/payment/notices/service/service/impl/NoticeGenerationServiceImpl.java
@@ -106,7 +106,8 @@ public class NoticeGenerationServiceImpl implements NoticeGenerationService {
 
         String ciTaxCode = noticeGenerationRequestItem.getData().getCreditorInstitution().getTaxCode();
 
-        if(!userId.equals(ciTaxCode) && !brokerService.checkBrokerAllowance(userId, ciTaxCode,
+        if (!"ADMIN".equals(userId) &&
+                !userId.equals(ciTaxCode) && !brokerService.checkBrokerAllowance(userId, ciTaxCode,
                 noticeGenerationRequestItem.getData().getNotice().getCode())) {
             throw new AppException(AppError.NOT_ALLOWED_ON_CI_CODE);
         }
