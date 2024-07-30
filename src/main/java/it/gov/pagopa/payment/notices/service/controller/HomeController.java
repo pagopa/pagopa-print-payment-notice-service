53c8bf261ce00fdcc4c2d084c9596356b2fa21b4
Jacopo <jacopo.carlini@gmail.com>
2024-07-30 10:38:41 +0200

    health check

--- src/main/java/it/gov/pagopa/payment/notices/service/controller/HomeController.java
+++ src/main/java/it/gov/pagopa/payment/notices/service/controller/HomeController.java
@@ -1,9 +1,15 @@
 package it.gov.pagopa.payment.notices.service.controller;
 
 import io.swagger.v3.oas.annotations.Hidden;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.security.SecurityRequirement;
+import it.gov.pagopa.payment.notices.service.model.AppInfo;
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.servlet.view.RedirectView;
 
@@ -14,6 +20,15 @@ public class HomeController {
     @Value("${server.servlet.context-path}")
     String basePath;
 
+    @Value("${info.application.name}")
+    private String name;
+
+    @Value("${info.application.version}")
+    private String version;
+
+    @Value("${info.properties.environment}")
+    private String environment;
+
 
     /**
      * @return redirect to Swagger page documentation
@@ -21,10 +36,19 @@ public class HomeController {
     @Hidden
     @GetMapping("")
     public RedirectView home() {
-        if(!basePath.endsWith("/")) {
+        if (!basePath.endsWith("/")) {
             basePath += "/";
         }
         return new RedirectView(basePath + "swagger-ui.html");
+    }
+
+    @Operation(summary = "health check", description = "Return OK if application is started", security = {@SecurityRequirement(name = "JWT")}, tags = {"Home"})
+    @GetMapping(value = "/info")
+    @ResponseStatus(HttpStatus.OK)
+    public ResponseEntity<AppInfo> healthCheck() {
+        // Used just for health checking
+        AppInfo info = AppInfo.builder().name(name).version(version).environment(environment).build();
+        return ResponseEntity.status(HttpStatus.OK).body(info);
     }
 
 }
