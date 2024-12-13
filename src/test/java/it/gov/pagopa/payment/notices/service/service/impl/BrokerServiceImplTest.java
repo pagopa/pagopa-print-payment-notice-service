0539fa7918470dbeed264ef477c50837321b1952
Jacopo Carlini <jacopo.carlini@pagopa.it>
2024-12-13 18:27:00 +0100

    test: resolve code smells

--- src/test/java/it/gov/pagopa/payment/notices/service/service/impl/BrokerServiceImplTest.java
+++ src/test/java/it/gov/pagopa/payment/notices/service/service/impl/BrokerServiceImplTest.java
@@ -19,88 +19,135 @@ import org.mockito.junit.jupiter.MockitoExtension;
 @ExtendWith(MockitoExtension.class)
 class BrokerServiceImplTest {
 
-    @Mock
-    public ApiConfigClient apiConfigClient;
+  @Mock public ApiConfigClient apiConfigClient;
 
-    private BrokerServiceImpl brokerService;
+  private BrokerServiceImpl brokerService;
 
-    @BeforeEach
-    public void init() {
-        Mockito.reset(apiConfigClient);
-        brokerService = new BrokerServiceImpl(apiConfigClient, 2);
-    }
+  @BeforeEach
+  public void init() {
+    Mockito.reset(apiConfigClient);
+    brokerService = new BrokerServiceImpl(apiConfigClient, 2);
+  }
 
-    @Test
-    public void shouldReturnFalseOnNullList() {
-        boolean result = brokerService.checkBrokerAllowance(
-                "test","test","3202323");
-        assertFalse(result);
-        verify(apiConfigClient).getCreditorInstitutionsAssociatedToBrokerStations(any(),any(),any(),any());
-    }
+  @Test
+  void shouldReturnFalseOnNullList() {
+    boolean result = brokerService.checkBrokerAllowance("test", "test", "3202323");
+    assertFalse(result);
+    verify(apiConfigClient)
+        .getCreditorInstitutionsAssociatedToBrokerStations(any(), any(), any(), any());
+  }
 
-    @Test
-    public void shouldReturnFalseOnEmptyList() {
-        doReturn(CreditorInstitutionsView.builder().creditorInstitutionList(Collections.emptyList()).build())
-                .when(apiConfigClient).getCreditorInstitutionsAssociatedToBrokerStations(any(),any(),any(),any());
-        boolean result = brokerService.checkBrokerAllowance(
-                "test","test","3202323");
-        assertFalse(result);
-        verify(apiConfigClient).getCreditorInstitutionsAssociatedToBrokerStations(any(),any(),any(),any());
-    }
+  @Test
+  void shouldReturnFalseOnEmptyList() {
+    doReturn(
+            CreditorInstitutionsView.builder()
+                .creditorInstitutionList(Collections.emptyList())
+                .build())
+        .when(apiConfigClient)
+        .getCreditorInstitutionsAssociatedToBrokerStations(any(), any(), any(), any());
+    boolean result = brokerService.checkBrokerAllowance("test", "test", "3202323");
+    assertFalse(result);
+    verify(apiConfigClient)
+        .getCreditorInstitutionsAssociatedToBrokerStations(any(), any(), any(), any());
+  }
 
-    @Test
-    public void shouldReturnTrueOnValidElementOnList_applicationCode() {
-        doReturn(CreditorInstitutionsView.builder().creditorInstitutionList(Collections.singletonList(
-                CreditorInstitutionView.builder().auxDigit(null).progressivo(2L).segregazione(1L).build()
-        )).build()).when(apiConfigClient).getCreditorInstitutionsAssociatedToBrokerStations(any(),any(),any(),any());
-        boolean result = brokerService.checkBrokerAllowance(
-                "test","test","30242323");
-        assertTrue(result);
-        verify(apiConfigClient).getCreditorInstitutionsAssociatedToBrokerStations(any(),any(),any(),any());
-    }
+  @Test
+  void shouldReturnTrueOnValidElementOnList_applicationCode() {
+    doReturn(
+            CreditorInstitutionsView.builder()
+                .creditorInstitutionList(
+                    Collections.singletonList(
+                        CreditorInstitutionView.builder()
+                            .auxDigit(null)
+                            .progressivo(2L)
+                            .segregazione(1L)
+                            .build()))
+                .build())
+        .when(apiConfigClient)
+        .getCreditorInstitutionsAssociatedToBrokerStations(any(), any(), any(), any());
+    boolean result = brokerService.checkBrokerAllowance("test", "test", "30242323");
+    assertTrue(result);
+    verify(apiConfigClient)
+        .getCreditorInstitutionsAssociatedToBrokerStations(any(), any(), any(), any());
+  }
 
-    @Test
-    public void shouldReturnTrueOnValidElementOnList_segregationCode() {
-        doReturn(CreditorInstitutionsView.builder().creditorInstitutionList(Collections.singletonList(
-                CreditorInstitutionView.builder().auxDigit(null).progressivo(null).segregazione(19L).build()
-        )).build()).when(apiConfigClient).getCreditorInstitutionsAssociatedToBrokerStations(any(),any(),any(),any());
-        boolean result = brokerService.checkBrokerAllowance(
-                "test","test","31942323");
-        assertTrue(result);
-        verify(apiConfigClient).getCreditorInstitutionsAssociatedToBrokerStations(any(),any(),any(),any());
-    }
+  @Test
+  void shouldReturnTrueOnValidElementOnList_segregationCode() {
+    doReturn(
+            CreditorInstitutionsView.builder()
+                .creditorInstitutionList(
+                    Collections.singletonList(
+                        CreditorInstitutionView.builder()
+                            .auxDigit(null)
+                            .progressivo(null)
+                            .segregazione(19L)
+                            .build()))
+                .build())
+        .when(apiConfigClient)
+        .getCreditorInstitutionsAssociatedToBrokerStations(any(), any(), any(), any());
+    boolean result = brokerService.checkBrokerAllowance("test", "test", "31942323");
+    assertTrue(result);
+    verify(apiConfigClient)
+        .getCreditorInstitutionsAssociatedToBrokerStations(any(), any(), any(), any());
+  }
 
-    @Test
-    public void shouldReturnTrueOnValidElementOnList_auxDigit1() {
-        doReturn(CreditorInstitutionsView.builder().creditorInstitutionList(Collections.singletonList(
-                CreditorInstitutionView.builder().auxDigit(1L).progressivo(null).segregazione(19L).build()
-        )).build()).when(apiConfigClient).getCreditorInstitutionsAssociatedToBrokerStations(any(),any(),any(),any());
-        boolean result = brokerService.checkBrokerAllowance(
-                "test","test","18842323");
-        assertTrue(result);
-        verify(apiConfigClient).getCreditorInstitutionsAssociatedToBrokerStations(any(),any(),any(),any());
-    }
+  @Test
+  void shouldReturnTrueOnValidElementOnList_auxDigit1() {
+    doReturn(
+            CreditorInstitutionsView.builder()
+                .creditorInstitutionList(
+                    Collections.singletonList(
+                        CreditorInstitutionView.builder()
+                            .auxDigit(1L)
+                            .progressivo(null)
+                            .segregazione(19L)
+                            .build()))
+                .build())
+        .when(apiConfigClient)
+        .getCreditorInstitutionsAssociatedToBrokerStations(any(), any(), any(), any());
+    boolean result = brokerService.checkBrokerAllowance("test", "test", "18842323");
+    assertTrue(result);
+    verify(apiConfigClient)
+        .getCreditorInstitutionsAssociatedToBrokerStations(any(), any(), any(), any());
+  }
 
-    @Test
-    public void shouldReturnTrueOnValidElementOnList_auxDigit2() {
-        doReturn(CreditorInstitutionsView.builder().creditorInstitutionList(Collections.singletonList(
-                CreditorInstitutionView.builder().auxDigit(2L).progressivo(null).segregazione(19L).build()
-        )).build()).when(apiConfigClient).getCreditorInstitutionsAssociatedToBrokerStations(any(),any(),any(),any());
-        boolean result = brokerService.checkBrokerAllowance(
-                "test","test","28842323");
-        assertTrue(result);
-        verify(apiConfigClient).getCreditorInstitutionsAssociatedToBrokerStations(any(),any(),any(),any());
-    }
+  @Test
+  void shouldReturnTrueOnValidElementOnList_auxDigit2() {
+    doReturn(
+            CreditorInstitutionsView.builder()
+                .creditorInstitutionList(
+                    Collections.singletonList(
+                        CreditorInstitutionView.builder()
+                            .auxDigit(2L)
+                            .progressivo(null)
+                            .segregazione(19L)
+                            .build()))
+                .build())
+        .when(apiConfigClient)
+        .getCreditorInstitutionsAssociatedToBrokerStations(any(), any(), any(), any());
+    boolean result = brokerService.checkBrokerAllowance("test", "test", "28842323");
+    assertTrue(result);
+    verify(apiConfigClient)
+        .getCreditorInstitutionsAssociatedToBrokerStations(any(), any(), any(), any());
+  }
 
-    @Test
-    public void shouldReturnFalseOnInvalidElement() {
-        doReturn(CreditorInstitutionsView.builder().creditorInstitutionList(Collections.singletonList(
-                CreditorInstitutionView.builder().auxDigit(null).progressivo(null).segregazione(null).build()
-        )).build()).when(apiConfigClient).getCreditorInstitutionsAssociatedToBrokerStations(any(),any(),any(),any());
-        boolean result = brokerService.checkBrokerAllowance(
-                "test","test","3002323");
-        assertFalse(result);
-        verify(apiConfigClient).getCreditorInstitutionsAssociatedToBrokerStations(any(),any(),any(),any());
-    }
-
+  @Test
+  void shouldReturnFalseOnInvalidElement() {
+    doReturn(
+            CreditorInstitutionsView.builder()
+                .creditorInstitutionList(
+                    Collections.singletonList(
+                        CreditorInstitutionView.builder()
+                            .auxDigit(null)
+                            .progressivo(null)
+                            .segregazione(null)
+                            .build()))
+                .build())
+        .when(apiConfigClient)
+        .getCreditorInstitutionsAssociatedToBrokerStations(any(), any(), any(), any());
+    boolean result = brokerService.checkBrokerAllowance("test", "test", "3002323");
+    assertFalse(result);
+    verify(apiConfigClient)
+        .getCreditorInstitutionsAssociatedToBrokerStations(any(), any(), any(), any());
+  }
 }
