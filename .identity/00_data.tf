22ca67b91af158ed78c0800a5308540801c2d424
svariant <samuele.varianti@nttdata.com>
2025-04-23 11:02:49 +0200

    chore: [PIDM-480] Fix identity cd

--- .identity/00_data.tf
+++ .identity/00_data.tf
@@ -18,8 +18,8 @@ data "github_organization_teams" "all" {
 }
 
 data "azurerm_user_assigned_identity" "identity_cd_01" {
-  name                = "${local.prefix}-${var.env_short}-${local.domain}-01-github-cd-identity"
-  resource_group_name = "${local.prefix}-${var.env_short}-identity-rg"
+  resource_group_name = "${local.product}-identity-rg"
+  name                = "${local.product}-${local.domain}-job-01-github-cd-identity"
 }
 
 data "azurerm_user_assigned_identity" "identity_pr_01" {
